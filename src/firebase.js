7522c4b8f711262bd782c4e3846c4cb2750beaa7
santiago <santiagocrescimbeny@gmail.com>
2026-01-20 17:00:43 -0300

    refactor

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,14 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBIcmyimekWwoeOQruvwXSP2tSv5YYMv8s",
-    databaseURL: "https://orchar2026-default-rtdb.firebaseio.com",
     authDomain: "orchar2026.firebaseapp.com",
+    databaseURL: "https://orchar2026-default-rtdb.firebaseio.com",
     projectId: "orchar2026",
     storageBucket: "orchar2026.firebasestorage.app",
     messagingSenderId: "803581330019",
     appId: "1:803581330019:web:b52d369fe1e54854a02e56",
     measurementId: "G-LQ1ZMWREDH"
 };
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
